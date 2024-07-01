7ab689e2fe1695344664c9cabc810adfaf23592c
yuyunfrancis <francisberi@yahoo.com>
2024-07-01 19:19:17 +0200

    feat: appointment frontend

--- frontend/src/components/PatientDashboard/pages/appointment/Appointments.jsx
+++ frontend/src/components/PatientDashboard/pages/appointment/Appointments.jsx
@@ -92,7 +92,7 @@ const Appointments = () => {
             Book your first appointment to get started
           </p>
           <Link
-            to="/patient/therapist-lis"
+            to="/patient/therapist-list"
             className="bg-greenPrimary hover:bg-hoverColor text-white font-bold py-3 px-6 rounded inline-flex items-center transition duration-150 ease-in-out"
           >
             <FiPlus className="mr-2" />
@@ -110,7 +110,7 @@ const Appointments = () => {
           Appointments
         </h1>
         <Link
-          to="/patient/therapist-lis"
+          to="/patient/therapist-list"
           className="bg-greenPrimary hover:bg-hoverColor text-white font-bold py-2 px-4 rounded inline-flex items-center transition duration-150 ease-in-out"
         >
           <FiPlus className="mr-2" />
