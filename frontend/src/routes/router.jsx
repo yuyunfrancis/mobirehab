76e45d687a04f9dfd080ccfcf52e5857ad7ae852
yuyunfrancis <francisberi@yahoo.com>
2024-07-19 07:34:46 +0200

    fix: auth handling

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,10 +1,9 @@
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import PatientLogin from "../components/auth/patient/PatientLogin";
 import TherapistLogin from "../components/auth/therapist/TherapistLogin";
-// import AdminLogin from "../components/auth/admin/AdminLogin";
+// import AdminLogin from "../components/auth/admin/AdminLogin"
 import PatientDashboard from "../components/PatientDashboard/PatientDashboard";
-// import NotAuthorized from "../components/NotAuthorized";
-// import NotFound from "../components/NotFound"; // Assuming you have a NotFound component
 import PrivateRoutes from "./PrivateRoutes";
 import RedirectToDashboard from "./RedirectToDashboard";
 import NotAuthorized from "../pages/NotAuthorized";
@@ -12,53 +11,55 @@ import NotFound from "../pages/NotFound";
 import PatientSignup from "../components/auth/patient/PatientSignup";
 import TherapistDashboard from "../components/TherapistDashboard/TherapistDashboard";
 import TherapistSignup from "../components/auth/therapist/signup/TherapistSignup";
-import WelcomePage from "../pages/WelcomePage";
+import Loading from "../components/utilities/Loading";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<RedirectToDashboard />} />
-      <Route path="/welcome" element={<RedirectToDashboard />} />
-      <Route
-        path="/patient/login"
-        element={<PatientLogin END_POINT="patient/login" />}
-      />
-      <Route
-        path="/patient/signup"
-        element={<PatientSignup END_POINT="patient/signup" />}
-      />
-      <Route
-        path="/therapist/login"
-        element={<TherapistLogin END_POINT="therapist/login" />}
-      />
-      <Route
-        path="/therapist/signup"
-        element={<TherapistSignup END_POINT="therapist/signup" />}
-      />
-      <Route
-        path="/patient/*"
-        element={<PrivateRoutes allowedRoles={["patient"]} />}
-      >
-        <Route path="*" element={<PatientDashboard />} />
-      </Route>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<RedirectToDashboard />} />
+        <Route path="/welcome" element={<RedirectToDashboard />} />
+        <Route
+          path="/patient/login"
+          element={<PatientLogin END_POINT="patient/login" />}
+        />
+        <Route
+          path="/patient/signup"
+          element={<PatientSignup END_POINT="patient/signup" />}
+        />
+        <Route
+          path="/therapist/login"
+          element={<TherapistLogin END_POINT="therapist/login" />}
+        />
+        <Route
+          path="/therapist/signup"
+          element={<TherapistSignup END_POINT="therapist/signup" />}
+        />
+        <Route
+          path="/patient/*"
+          element={<PrivateRoutes allowedRoles={["patient"]} />}
+        >
+          <Route path="*" element={<PatientDashboard />} />
+        </Route>
 
-      <Route
-        path="/therapist/*"
-        element={<PrivateRoutes allowedRoles={["therapist"]} />}
-      >
-        <Route path="*" element={<TherapistDashboard />} />
-      </Route>
+        <Route
+          path="/therapist/*"
+          element={<PrivateRoutes allowedRoles={["therapist"]} />}
+        >
+          <Route path="*" element={<TherapistDashboard />} />
+        </Route>
 
-      {/* <Route
+        {/* <Route
         path="/admin/*"
         element={<PrivateRoutes allowedRoles={["admin"]} />}
       >
         <Route path="*" element={<AdminDashboard />} />
       </Route> */}
 
-      <Route path="/not-authorized" element={<NotAuthorized />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="/not-authorized" element={<NotAuthorized />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
 
